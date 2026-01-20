12da4f04909eedc695941c2fc84f5a1093cd4c9e
Jack Christensen <jack@jackmchristensen.com>
2026-01-20 17:18:22 -0600

    Update json file stats. Includes mesh statistics with applied modifiers

--- asset_forge.py
+++ asset_forge.py
@@ -3,6 +3,8 @@ from bpy.types import PropertyGroup
 from bpy.props import StringProperty
 import os
 import json
+import datetime
+
 
 class AF_Settings(PropertyGroup):
     export_dir: StringProperty(
@@ -13,7 +15,27 @@ class AF_Settings(PropertyGroup):
     )
 
 
-def get_active_object():
+# Returns mesh stats for mesh after modifiers are applied
+# Creates and clears temporary mesh to evaluate mesh statistics 
+def get_evaluated_mesh_stats(obj: bpy.types.Object, context) -> dict:
+    depsgraph = context.evaluated_depsgraph_get()
+    obj_eval = obj.evaluated_get(depsgraph)
+    
+    mesh_eval = obj_eval.to_mesh(preserve_all_data_layers=True, depsgraph=depsgraph)
+    
+    try:
+        verts = len(mesh_eval.vertices)
+        edges = len(mesh_eval.edges)
+        polys = len(mesh_eval.polygons)
+        
+        # Tri count always two less than vertex count per polygon
+        tri_count = sum(len(p.vertices) - 2 for p in mesh_eval.polygons)
+        return {"vertices": verts, "edges": edges, "faces": polys, "triangles": tri_count}
+    finally:
+        obj_eval.to_mesh_clear()
+
+
+def get_active_object() -> bpy.types.Object:
     obj = bpy.context.active_object
     
     if obj is None or obj.type != 'MESH':
@@ -45,15 +67,33 @@ def export_active_mesh_fbx(export_path: str):
     )
 
 
-def export_active_mesh_data(export_path: str):
+def export_active_mesh_data(export_path: str, context):
     obj = get_active_object()
+    stats = get_evaluated_mesh_stats(obj, context)
     
     os.makedirs(os.path.dirname(export_path), exist_ok=True)
     
     mesh_data = {
-        "mesh": [
-            {"name": obj.name, "vertices": len(obj.data.vertices)}
-        ]
+        "schema": "asset_forge.export",
+        "schema_version": "0.1.0",
+     
+        "source": {
+            "blend_file": bpy.data.filepath,
+            "object_name": obj.name
+        },
+        
+        "export": {
+            "target": "unreal",
+            "format": "fbx",
+            "export_path": export_path,
+            "export_dir": bpy.path.abspath(export_path),
+            "timestamp": datetime.datetime.now(tz=datetime.timezone.utc).strftime("%Y-%m-%dT:%H:%M:%SZ%z")
+        },
+        
+        "mesh": {
+            "name": obj.name,
+            "stats": { **stats }
+        }
     }
     
     with open(export_path, 'w') as f:
@@ -74,11 +114,11 @@ class AF_OT_export(bpy.types.Operator):
         object_export_path = os.path.join(export_dir, filename)
         
         obj_data = f"{obj.name}.json"
-        data_export_path= os.path.join(export_dir, obj_data)
+        data_export_path = os.path.join(export_dir, obj_data)
        
         try:
             export_active_mesh_fbx(object_export_path)
-            export_active_mesh_data(data_export_path)           
+            export_active_mesh_data(data_export_path, bpy.context)           
                 
         except Exception as e:
             self.report({"ERROR"}, str(e))
