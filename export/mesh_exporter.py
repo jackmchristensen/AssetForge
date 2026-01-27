821fb1cfdee9f180e16a925a5edcdadf29489bc1
Jack Christensen <jack@jackmchristensen.com>
2026-01-27 15:14:30 -0600

    Add docstrings.

--- export/mesh_exporter.py
+++ export/mesh_exporter.py
@@ -5,6 +5,10 @@ import json
 from typing import Any
 
 def export_active_mesh_fbx(export_path: str) -> None:
+    """Exports current active mesh as fbx. Creates export directory if 
+    one doesn't already exist.
+    """
+
     os.makedirs(os.path.dirname(export_path), exist_ok=True)
 
     bpy.ops.export_scene.fbx(
@@ -22,6 +26,10 @@ def export_active_mesh_fbx(export_path: str) -> None:
 
 
 def export_mesh_metadata(export_path: str, mesh_data: dict[str, Any]) -> None:
+    """Exports metadata as JSON file. Creates export directory if
+    one doesn't currently exist.
+    """
+
     os.makedirs(os.path.dirname(export_path), exist_ok=True)
 
     with open(export_path, "w") as f:
