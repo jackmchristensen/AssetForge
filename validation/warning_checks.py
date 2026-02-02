1f01cf922741fbdc3dbaf163186f2ef9833e414a
Jack Christensen <jack@jackmchristensen.com>
2026-02-02 16:00:09 -0600

    New temporary method to verify naming conventions
    Still needs to be written

--- validation/warning_checks.py
+++ validation/warning_checks.py
@@ -1,6 +1,12 @@
 import bpy
 
+from . import naming
+
 def validate_mesh_materials(obj: bpy.types.Object) -> bool:
     """Return true if object has materials"""
 
     return obj.type == "MESH" and bool(obj.data.materials)
+
+
+def validate_file_names() -> bool:
+    return True
